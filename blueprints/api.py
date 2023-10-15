25c197b2fd33db4904872174f2a1126f5d9d39d0
TEaMeNT <aleksandrovv40@gmail.com>
2023-10-15 23:16:41 +0300

    Вывод и ввод домашнего задания, смены дня

--- blueprints/api.py
+++ blueprints/api.py
@@ -8,9 +8,9 @@ api_blueprint = Blueprint('api', __name__)
 def api_get_daily_lessons():
     db = sqlite3.connect('bases/DB.db')
     cur = db.cursor()
-    days = cur.execute(f'''SELECT * FROM Schedule''').fetchall()
+    schedule = cur.execute(f'''SELECT * FROM Schedule''').fetchall()
     dict = {}
-    for day in days:
+    for day in schedule:
         if(day[0] in dict.keys()):
             dict[day[0]].append(day[1:])
         else:
@@ -18,3 +18,28 @@ def api_get_daily_lessons():
     cur.close()
     db.close()
     return json.dumps(dict)
+
+@api_blueprint.route("/api/homework_upd", methods=['GET', 'POST'])
+def homework_upd():
+    if request.method == "POST":
+        db = sqlite3.connect('bases/DB.db')
+        cur = db.cursor()
+        homework = json.loads(request.data)
+        print(homework)
+        cur.execute('INSERT INTO days (day, home_work) VALUES (?, ?)', (homework[0], homework[1]))
+        db.commit()
+        cur.close()
+        db.close()
+    return ''
+
+@api_blueprint.route("/api/get_day_homework", methods=['GET', 'POST'])
+def get_day_homework():
+    day = json.loads(request.data)
+    db = sqlite3.connect('bases/DB.db')
+    cur = db.cursor()
+    homework = []
+    for i in range(day[1]):
+        homework += [[i, cur.execute(f'''SELECT home_work FROM days WHERE day = "{day[0] + " " + str(i)}"''').fetchall()]]
+    cur.close()
+    db.close()
+    return json.dumps(homework)
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -20,36 +20,82 @@ function sendJSON(data, url){
     xhr.send(data);
 }
 
-const tooltip = document.getElementsByClassName("tooltip")[0];
+function parseDate(str){
+    var mdy = str.split("-");
+    return new Date(mdy[0], mdy[1] - 1, mdy[2]);
+}
+function datediff(first, second) {        
+    return Math.round((second - first) / (1000 * 60 * 60 * 24));
+}
+
+const tooltip = document.getElementById("tooltip");
 const tooltip_back = document.getElementsByClassName("tooltip_back")[0];
+const form = document.getElementById("form");
 
 const main_table = document.getElementsByClassName("main_table")[0];
+const date_selector = document.getElementsByClassName("date")[0];
+const arrow_left = document.getElementsByClassName("arrow")[0];
+const arrow_right = document.getElementsByClassName("arrow")[1];
 
 tooltip_back.onclick = function() {
     tooltip.classList.remove("_active");
     tooltip_back.classList.remove("_active");
 }
 
-loadJSON('api/api_get_daily_lessons',
-    function(data) {
-        let days = data;
-        var today = new Date();
+class Day {
+    constructor() {
+        this.current_day = new Date();
+        this.today_date = this.get_str_date(this.current_day);
+        this.week = this.get_week(this.today_date);
+        this.num_day = this.get_day(this.current_day, this.week);
+    }
+
+    get_str_date(today) {
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0');
         var yyyy = today.getUTCFullYear();
-        console.log(days);
-        console.log(dd, mm, yyyy);
-        
-        num_day = today.getDay();
-        let lessons = days[1];
+        return yyyy.toString() + "-" + mm.toString() + "-" + dd.toString()
+    }
+
+    get_week(today_date) {
+        return Math.floor(datediff(parseDate("2023-08-28"), parseDate(today_date)) / 7) + 1;
+    }
+
+    get_day(today, week) {
+        let ffuuuuu = new Date(today);
+        ffuuuuu.setDate(ffuuuuu.getDate() - 1);
+        return ((week % 2 == 0) + 1) * (ffuuuuu.getDay() + 1);
+    }
+
+    change_day(delta) {
+        this.current_day.setDate(this.current_day.getDate() + delta);
+        this.today_date = this.get_str_date(this.current_day);
+        this.week = this.get_week(this.today_date);
+        this.num_day = this.get_day(this.current_day, this.week);
+    }
+
+    get current_str_date() {
+        return this.today_date;
+    }
+
+    get current_num_day() {
+        return this.num_day;
+    }
+}
+
+function loadLessons(lessons) {
+    main_table.innerHTML = "";
+    date_selector.value = current_date.current_str_date;
+    if (lessons != null) {
         for (var i = 0; i < lessons.length; i++) {
-            console.log(main_table.childNodes);
-            main_table.insertAdjacentHTML('beforeend', `<div class="lesson_block">${lessons[i][0]}<div class="add_block"><div class="descr"><b>Преподаватель</b>: ${lessons[i][2]}</div><div class="descr"><b>Домашнее задание</b>: none</div><div class="descr"><b>Отмечает на лекциях</b>: ${lessons[i][3]}</div></div></div>`);
+            //console.log(main_table.childNodes);
+            main_table.insertAdjacentHTML('beforeend', `<div class="lesson_block"><div class="txt">${lessons[i][0]}</div><div class="add_block"><div class="descr"><b>Преподаватель</b>: ${lessons[i][2]}</div><div class="descr"><b>Домашнее задание</b>: none</div><div class="descr"><b>Отмечает на лекциях</b>: ${lessons[i][3]}</div></div></div>`);
         }
         let lesson_blocks = document.getElementsByClassName("lesson_block");
         for (let i = 0; i < lesson_blocks.length; i++) {
             let add_block = lesson_blocks[i].childNodes[1];
-            lesson_blocks[i].onclick = function () {
+            let homework = add_block.childNodes[1];
+            lesson_blocks[i].firstChild.onclick = function () {
                 let sum_of_heights = 0;
                 for (let j = 0; j < add_block.childNodes.length; j++) {
                     let height = add_block.childNodes[j].offsetHeight;
@@ -66,7 +112,68 @@ loadJSON('api/api_get_daily_lessons',
                     add_block.style["max-height"] = correct_h;
                 }
             }
+            homework.onclick = function() {
+                tooltip.classList.add("_active");
+                tooltip_back.classList.add("_active");
+                tooltip.childNodes[3].onclick = function() {
+                    var data_to_send = [date_selector.value + " " + i, form.value];
+                    sendJSON(JSON.stringify(data_to_send), "/api/homework_upd");
+                    homework.innerHTML = "<b>Домашнее задание</b>: " + form.value;
+                }
+            }
         }
+    }
+    else {
+        main_table.innerHTML = "No lessons";
+    }
+}
 
+function loadHomework() {
+    let lesson_blocks = document.getElementsByClassName("lesson_block");
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/get_day_homework", true);
+    xhr.send(JSON.stringify([date_selector.value, lesson_blocks.length]));
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                data = JSON.parse(xhr.responseText);
+                let lesson_blocks = document.getElementsByClassName("lesson_block");
+                for (let i = 0; i < data.length; i++) {
+                    lesson_blocks[data[i][0]].childNodes[1].childNodes[1].innerHTML = "<b>Домашнее задание</b>: " + data[i][1];
+                }
+            }
+        }
+    };
+}
+
+loadJSON('api/api_get_daily_lessons',
+    function(data) {
+        let days = data;
+        current_date = new Day();
+
+        date_selector.value = current_date.current_str_date;
+        var lessons = days[current_date.current_num_day];
+
+        arrow_left.onclick = function() {
+            current_date.change_day(-1);
+            lessons = days[current_date.current_num_day];
+            loadLessons(lessons);
+            loadHomework();
+        }
+        arrow_right.onclick = function() {
+            current_date.change_day(1);
+            lessons = days[current_date.current_num_day];
+            loadLessons(lessons);
+            loadHomework();
+        }
+        date_selector.addEventListener('change', function(e) {
+            current_date.change_day(datediff(parseDate(current_date.current_str_date), parseDate(this.value)));
+            lessons = days[current_date.current_num_day];
+            loadLessons(lessons);
+            loadHomework();
+        });
+
+        loadLessons(lessons);
+        loadHomework();
     }
 )
